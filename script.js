4502c95b3291dc43e5a168d072d63c6865b69bf6
anshika15jaiswal-ctrl <anshika15jaiswal@gmail.com>
2026-07-18 11:23:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function showMessage() {
-  alert("Welcome to Titanexchange!");
-}
+document.addEventListener("DOMContentLoaded", function () {
+
+    const loginButton = document.querySelector(".hero button");
+
+    loginButton.addEventListener("click", function () {
+        alert("Login page is coming soon!");
+    });
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(function(card){
+        card.addEventListener("click", function(){
+            alert("Match details will be available soon.");
+        });
+    });
+
+});
